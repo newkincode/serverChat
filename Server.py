afcedda007031266de6f3fe6cfe50eb22d0abae9
newkincode <maainnewkin59@gmail.com>
2023-04-07 23:29:05 +0900

    돼지만 한번 보낸후 다른사람이 보낼때까지 기다려야함

--- Server.py
+++ Server.py
@@ -48,23 +48,6 @@ def handle(client):
     while True:
         try:
             msg = message = client.recv(1024)
-            if msg.decode('ascii').startswith('KICK'):
-                if nicknames[clients.index(client)] == 'admin':
-                    name_to_kick = msg.decode('ascii')[5:]
-                    kick_user(name_to_kick)
-                else:
-                    client.send('Command Refused!'.encode('ascii'))
-            elif msg.decode('ascii').startswith('BAN'):
-                if nicknames[clients.index(client)] == 'admin':
-                    name_to_ban = msg.decode('ascii')[4:]
-                    kick_user(name_to_ban)
-                    with open('bans.txt', 'a') as f:
-                        f.write(f'{name_to_ban}\n')
-                    print(f'{name_to_ban} was banned by the Admin!')
-                else:
-                    client.send('Command Refused!'.encode('ascii'))
-            else:
-                broadcast(message)  # As soon as message received, broadcast it.
 
         except socket.error:
             if client in clients:
@@ -86,23 +69,6 @@ def receive():
         # Ask the clients for Nicknames
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-        # If the Client is an Admin prompt for the password.
-        with open('bans.txt', 'r') as f:
-            bans = f.readlines()
-
-        if nickname + '\n' in bans:
-            client.send('BAN'.encode('ascii'))
-            client.close()
-            continue
-
-        if nickname == 'admin':
-            client.send('PASS'.encode('ascii'))
-            password = client.recv(1024).decode('ascii')
-            # I know it is lame, but my focus is mainly for Chat system and not a Login System
-            if password != 'adminpass':
-                client.send('REFUSE'.encode('ascii'))
-                client.close()
-                continue
 
         nicknames.append(nickname)
         clients.append(client)
@@ -115,16 +81,6 @@ def receive():
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-
-def kick_user(name):
-    if name in nicknames:
-        name_index = nicknames.index(name)
-        client_to_kick = clients[name_index]
-        clients.remove(client_to_kick)
-        client_to_kick.send('You Were Kicked from Chat !'.encode('ascii'))
-        client_to_kick.close()
-        nicknames.remove(name)
-        broadcast(f'{name} was kicked from the server!'.encode('ascii'))
 
 
 # Calling the main method
