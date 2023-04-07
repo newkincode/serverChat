d0f1c88d1913d87b139d6d5ca0e3baa39f125a38
newkincode <maainnewkin59@gmail.com>
2023-04-07 23:22:49 +0900

    클라이언트 코드 수정완료

--- Client.py
+++ Client.py
@@ -1,4 +1,7 @@
 import socket
+import threading
+import json
+import os
 
 RESET = "\u001B[0m"
 FONT_BLACK = "\u001B[30m"
@@ -17,104 +20,17 @@ BACKGROUND_BLUE = "\u001B[44m"
 BACKGROUND_PURPLE = "\u001B[45m"
 BACKGROUND_CYAN = "\u001B[46m"
 BACKGROUND_WHITE = "\u001B[47m"
-
-print("1. conect server")
-print("2. add server list")
-print("3. opnen server")
-num = int(input("select: "))
-print("\033[2J\033[H")
+stop_thread = False
 
 # from https://github.com/IamLucif3r/Chat-On/blob/main/client.py
-import socket
-import threading
-import json
-import os
-
-
 def enter_server():
     os.system('cls||clear')
-    # Enter servers.json to print the names of the servers
-    data = {
-    "Example": {
-        "ip": "127.0.0.1",
-        "port": 5555
-    }
-}
-    print('Your servers: ', end="")
-    # Print the servers that are stored in the servers.json file
-    for servers in data:
-        print(servers, end=" ")
-    # Ask user for the name of the server to join
-    server_name = input("\nEnter the server name:")
-    global nickname
-    global password
-    nickname = input("Choose Your Nickname:")
-    if nickname == 'admin':
-        password = input("Enter Password for Admin:")
-
-    # Store the ip and port number for connection
-    ip = data[server_name]["ip"]
-    port = data[server_name]["port"]
+    ip = input("server ip : ")
+    port = int(input("server port : "))
     global client
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to a host
     client.connect((ip, port))
-
-
-def add_server():
-    os.system('cls||clear')
-    server_name = input("Enter a name for the server:")
-    server_ip = input("Enter the ip address of the server:")
-    server_port = int(input("Enter the port number of the server:"))
-
-    with open('servers.json', 'r') as f:
-        data = json.load(f)
-    # Store the info of the new server in servers.json
-    with open('servers.json', 'w') as f:
-        data[server_name] = {"ip": server_ip, "port": server_port}
-        json.dump(data, f, indent=4)
-
-
-# Menu loop, it will loop until the user choose to enter a server
-while True:
-    os.system('cls||clear')
-    option = input("(1)Enter server\n(2)Add server\n")
-    if option == '1':
-        enter_server()
-        break
-    elif option == '2':
-        add_server()
-
-stop_thread = False
-
-
-def receive():
-    while True:
-        global stop_thread
-        if stop_thread:
-            break
-        try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
-                next_message = client.recv(1024).decode('ascii')
-                if next_message == 'PASS':
-                    client.send(password.encode('ascii'))
-                    if client.recv(1024).decode('ascii') == 'REFUSE':
-                        print("Connection is Refused !! Wrong Password")
-                        stop_thread = True
-                # Clients those are banned can't reconnect
-                elif next_message == 'BAN':
-                    print('Connection Refused due to Ban')
-                    client.close()
-                    stop_thread = True
-            else:
-                print(message)
-        except socket.error:
-            print('Error Occured while Connecting')
-            client.close()
-            break
-
 
 def write():
     while True:
@@ -122,21 +38,16 @@ def write():
             break
         # Getting Messages
         message = f'{nickname}: {input("")}'
-        if message[len(nickname) + 2:].startswith('/'):
-            if nickname == 'admin':
-                if message[len(nickname) + 2:].startswith('/kick'):
-                    # 2 for : and whitespace and 6 for /KICK_
-                    client.send(f'KICK {message[len(nickname) + 2 + 6:]}'.encode('ascii'))
-                elif message[len(nickname) + 2:].startswith('/ban'):
-                    # 2 for : and whitespace and 5 for /BAN
-                    client.send(f'BAN {message[len(nickname) + 2 + 5:]}'.encode('ascii'))
-            else:
-                print("Commands can be executed by Admins only !!")
-        else:
-            client.send(message.encode('ascii'))
+        client.send(message.encode('ascii'))
+# by nekwini
+print("1. conect server")
+print("2. add server list")
+print("3. opnen server")
+num = int(input("select: "))
+print("\033[2J\033[H")
 
-
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-write_thread = threading.Thread(target=write)
-write_thread.start()
+if num == 1:
+    nickname = input("your name:")
+    enter_server()
+    write_thread = threading.Thread(target=write)
+    write_thread.start()
