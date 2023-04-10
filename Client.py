e34ab2831219e9c2bd61fd3d109a5bd20ef0928a
newkincode <maainnewkin59@gmail.com>
2023-04-10 22:38:25 +0900

    챗gpt는 ai야

--- Client.py
+++ Client.py
@@ -1,53 +1,34 @@
 import socket
 import threading
-import json
-import os
 
-RESET = "\u001B[0m"
-FONT_BLACK = "\u001B[30m"
-FONT_RED = "\u001B[31m"
-FONT_GREEN = "\u001B[32m"
-FONT_YELLOW = "\u001B[33m"
-FONT_BLUE = "\u001B[34m"
-FONT_PURPLE = "\u001B[35m"
-FONT_CYAN = "\u001B[36m"
-FONT_WHITE = "\u001B[37m"
-BACKGROUND_BLACK = "\u001B[40m"
-BACKGROUND_RED = "\u001B[41m"
-BACKGROUND_GREEN = "\u001B[42m"
-BACKGROUND_YELLOW = "\u001B[43m"
-BACKGROUND_BLUE = "\u001B[44m"
-BACKGROUND_PURPLE = "\u001B[45m"
-BACKGROUND_CYAN = "\u001B[46m"
-BACKGROUND_WHITE = "\u001B[47m"
-stop_thread = False
+HOST = '127.0.0.1'
+PORT = 9999
 
-# from https://github.com/IamLucif3r/Chat-On/blob/main/client.py
-def enter_server():
-    os.system('cls||clear')
-    ip = input("server ip : ")
-    port = int(input("server port : "))
-    global client
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Connect to a host
-    client.connect((ip, port))
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
 
 def write():
     while True:
-        if stop_thread:
-            break
-        # Getting Messages
         message = f'{nickname}: {input("")}'
         client.send(message.encode('ascii'))
-# by nekwini
-print("1. conect server")
-print("2. add server list")
-print("3. opnen server")
-num = int(input("select: "))
-print("\033[2J\033[H")
 
-if num == 1:
-    nickname = input("your name:")
-    enter_server()
-    write_thread = threading.Thread(target=write)
-    write_thread.start()
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- Server.py
+++ Server.py
@@ -1,88 +1,51 @@
 import socket
-
-def opne():
-    print("server")
-    ip = "127.0.0.1"
-    port = int(input("port: "))
-    
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((ip, port))
-    server_socket.listen()
-    while True:
-        client_socket, addr = server_socket.accept()
-        print('conect: ', addr)
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        print('received: ', addr, data.decode())
-        client_socket.sendall(data)
-
-# from https://github.com/IamLucif3r/Chat-On/blob/main/server.py
 import threading
-import socket
 
-# Now this Host is the IP address of the Server, over which it is running.
-# I've user my localhost.
-host = "127.0.0.1"
-port = 5555  # Choose any random port which is not so common (like 80)
+HOST = '127.0.0.1'
+PORT = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the server to IP Address
-server.bind((host, port))
-# Start Listening Mode
+server.bind((HOST, PORT))
 server.listen()
-# List to contain the Clients getting connected and nicknames
+
 clients = []
-nicknames = []
 
-
-# 1.Broadcasting Method
 def broadcast(message):
     for client in clients:
         client.send(message)
 
-
-# 2.Receiving Messages from client then broadcasting
-def handle(client):
+def handle_client(client):
     while True:
         try:
-            msg = message = client.recv(1024)
-
+            message = client.recv(1024)
+            broadcast(message)
         except:
-            if client in clients:
-                index = clients.index(client)
-                # Index is used to remove client from list after getting disconnected
-                client.remove(client)
-                client.close()
-                nickname = nicknames[index]
-                broadcast(f'{nickname} left the Chat!'.encode('ascii'))
-                nicknames.remove(nickname)
-                break
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
 
-
-# Main Receive method
 def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-        # Ask the clients for Nicknames
+
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f'Nickname of the client is {nickname}')
-        broadcast(f'{nickname} joined the Chat'.encode('ascii'))
-        client.send('Connected to the Server!'.encode('ascii'))
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send('Connected to the server!'.encode('ascii'))
 
-        # Handling Multiple Clients Simultaneously
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
 
+nicknames = []
 
-
-# Calling the main method
-print('Server is Listening ...')
+print("Server is running...")
 receive()
